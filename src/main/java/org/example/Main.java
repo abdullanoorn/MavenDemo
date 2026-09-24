8af6fd16cdc6d74243db9df53dc30bcd4a0ebfc1
Abdulla <abdulla.basha@sritindia.com>
2026-09-24 12:36:25 +0530

    Update Main program

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Maven!");
+        System.out.println("Git change practice");
     }
 }
