2bd58bdcaf5968f101c6da4222abc7ace08638f2
Abdulla <abdulla.basha@sritindia.com>
2026-09-24 13:53:18 +0530

    Add feature branch practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Git change practice");
+        System.out.println("Git feature branch practice");
     }
 }
